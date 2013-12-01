a22938da8c3fb325af257e94c6287ca74dc7c175
qhong <frederickhong.5@gmail.com>
2013-12-01 04:26:34 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gostore::Application.config.secret_token = 'a31efbdea0c8e220098cec9a697db9b90954f7357472fdd7cd1f7fa83ac4c40239376eeb51926a1ac681e9ab2435f66bc9e39a114d645e3dea0d4cd490518023'
