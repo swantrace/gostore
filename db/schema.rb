388c32f9bf235fcd4fe884831d1c136e3234ff4a
qhong <frederickhong.5@gmail.com>
2013-12-01 06:47:56 -0600

    new product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131201115244) do
+ActiveRecord::Schema.define(:version => 20131201123739) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
